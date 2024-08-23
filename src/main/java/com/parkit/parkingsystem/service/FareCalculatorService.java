45b6ec8a600f9db243d3ec7347669ede7e5b188b
nicolas cretton <nicolas.cretton@orange.fr>
2024-08-24 00:26:59 +0200

    Ajout de la fonctionnalité des 30 minutes gratuites

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -12,9 +12,12 @@ public class FareCalculatorService {
 
 		long inHour = ticket.getInTime().getTime();
 		long outHour = ticket.getOutTime().getTime();
-
-		// TODO: Some tests are failing here. Need to check if this logic is correct
 		double duration = (outHour - inHour) / (1000.0 * 60 * 60);
+
+		if (duration <= 0.5) {
+			ticket.setPrice(0);
+			return;
+		}
 
 		switch (ticket.getParkingSpot().getParkingType()) {
 		case CAR: {
